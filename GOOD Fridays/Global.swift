567b4f136383e014ff4ed8be5ba4491a62ec20e6
Ian Hirschfeld <ianh7976@gmail.com>
2016-01-25 18:35:28 -0800

    Starts prep for smarter global audio player.

---
+++ GOOD Fridays/Global.swift
@@ -0,0 +1,15 @@
+//
+//  Global.swift
+//  GOOD Fridays
+//
+//  Created by Ian Hirschfeld on 1/25/16.
+//  Copyright © 2016 The Soap Collective. All rights reserved.
+//
+
+import UIKit
+
+struct Global {
+
+  static var isPlaying = false
+
+}
